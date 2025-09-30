ab7804e6a700a3f678f6cd289ae32741dbd0a7d6
Carfy4 <carfbobrik33@gmail.com>
2025-09-30 17:19:18 +0300

    Changed text position

--- components/Experience.tsx
+++ components/Experience.tsx
@@ -4,15 +4,15 @@ import React from "react";
 const Experience: React.FC<{}> = () => {
   return (
     <section id="experience">
-      <div className="container mx-auto px-4"> {/* Добавьте этот контейнер */}
+      <div className="container mx-auto px-4">
         <h2 className="text-white font-semibold text-center text-6xl pt-[35px]">
           WHAT I&apos;M EXPERIENCED IN
         </h2>
         <p className="tracking-[0.5em] text-center text-transparent font-light pb-5 bg-clip-text bg-gradient-to-r from-purple-700 to-orange-500 text-1xl">
           
         </p>
-        <div className="max-w-4xl mx-auto"> {/* Или этот для ограничения ширины текста */}
-          <p className="text-gray-300 pt-5 leading-8 text-left">
+        <div className="max-w-4xl mx-auto">
+          <p className="text-gray-300 pt-5 leading-8 text-center">
             DataStore / ProfileService<br />
             UI Scripting (daily rewards, playtime rewards, egg & pet systems)<br />
             Simulators, Tycoons, Obbies and more<br />
